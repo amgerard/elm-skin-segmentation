4ab33cc44016f00df48c474a4a60b0fdf6c8fda5
Alex Gerard <amgerard@amgerard.local>
2016-11-19 05:55:14 -0600

    accuracy instead of error

--- testing/test3.py
+++ testing/test3.py
@@ -3,6 +3,7 @@ start = time.time()
 
 import numpy as np
 from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import accuracy_score
 from ELM import ELMRegressor
 
 # same transformed data
@@ -11,15 +12,17 @@ y_train = np.loadtxt("y_train.csv", delimiter=",")
 X_test = np.loadtxt("X_test.csv", delimiter=",")
 y_test = np.loadtxt("y_test.csv", delimiter=",")
 
-for x in range(500, 10000, 500):
+for x in range(500, 2000, 200):
 	ELM = ELMRegressor(x)
 	ELM.fit(X_train, y_train)
 	prediction = ELM.predict(X_train)
 	print x
-	print 'train error: ' + str(mean_absolute_error(y_train, prediction))
+	# print 'train error: ' + str(mean_absolute_error(y_train, prediction))
+	print 'train accuracy: ' + str(accuracy_score(y_train, prediction))
 
 	prediction = ELM.predict(X_test)
-	print 'test error: ' + str(mean_absolute_error(y_test, prediction))
+	#print 'test error: ' + str(mean_absolute_error(y_test, prediction))
+	print 'test accuracy: ' + str(accuracy_score(y_test, prediction))
 
 	end = time.time()
 	print 'time elapsed: ' + str(end-start)
