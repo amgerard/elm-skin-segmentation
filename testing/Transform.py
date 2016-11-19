bee3b00482f6dfed1231e0985e2c73edf83a6177
Alex Gerard <amgerard@amgerard.local>
2016-11-19 05:04:55 -0600

    save transformed data

--- testing/Transform.py
+++ testing/Transform.py
@@ -15,109 +15,106 @@ from skimage.util import img_as_float
 
 #class ITransform(metaclass=ABCMeta):
 class ITransform():
-    def __init__(self, images, masks):
-        self.images=images
-	self.masks=masks
-	self.X = np.ones(1);
-	self.y = np.ones(1);
-    @abstractmethod
-    def transform(self):
-        pass
-    def im_2_mask(self,im):
-        '''
-        covert mask to 1's and 0's and reshape
-        '''
-        mu = np.mean(im, axis=2)
-        mask2d = mu < 255
-        return mask2d.astype(int)
+	def __init__(self, images, masks):
+		self.images=images
+		self.masks=masks
+		self.X = np.ones(1);
+		self.y = np.ones(1);
+	@abstractmethod
+	def transform(self):
+		pass
+	def im_2_mask(self,im):
+		'''
+		covert mask to 1's and 0's and reshape
+		'''
+		mu = np.mean(im, axis=2)
+		mask2d = mu < 255
+		return mask2d.astype(int)
 
 class SimpleTransform(ITransform):
-    def transform(self):
-        self.X = np.concatenate([self.im_reshape(im) for im in self.images], axis=0)
-        self.y = np.concatenate([self.im_2_mask(im) for im in self.masks], axis=0)
-    def im_reshape(self,im):
-        '''
-        reshape so each pixel is a (r,b,g) row
-        '''
-        x,y,z = im.shape
-        return im.reshape(x*y,z)
-    def im_2_mask(self,im):
-        mask2d = ITransform.im_2_mask(im)
-        x,y = mask2d.shape
-        return mask2d.reshape(x*y)
+	def transform(self):
+		self.X = np.concatenate([self.im_reshape(im) for im in self.images], axis=0)
+		self.y = np.concatenate([self.im_2_mask(im) for im in self.masks], axis=0)
+	def im_reshape(self,im):
+		'''
+		reshape so each pixel is a (r,b,g) row
+		'''
+		x,y,z = im.shape
+		return im.reshape(x*y,z)
+	def im_2_mask(self,im):
+		mask2d = ITransform.im_2_mask(im)
+		x,y = mask2d.shape
+		return mask2d.reshape(x*y)
 
 class SuperPxlTransform(ITransform):
-    def transform(self):
-        #images_and_masks = zip(self.images, [self.im_2_mask(m) for m in self.masks])
-        #print len(test[0])
-	st = time.time()
-        self.y =[]
-        pool = ThreadPool(20) 
+	def transform(self):
+		im_labels = [get_im_labels(im) for im in self.images]
+		ims_and_labels = zip(self.images, im_labels)
 
-        #results = pool.map(self.im_superpixels, images_and_masks)
-        im_labels = pool.map(self.get_im_labels, self.images)
-        pool.close() 
-        pool.join() # wait 
+		results = [self.im_superpixels(pair) for pair in ims_and_labels]
+		self.X = np.concatenate(results, axis=0)
 
-	print '1 => ' + str(time.time()-st)
+		labels_and_masks = zip(im_labels, [self.im_2_mask(m) for m in self.masks])
+		yy = [im_mask(pair) for pair in labels_and_masks]
+		self.y = np.concatenate(yy, axis=0)
+	
+	def get_im_labels(self,im):
+		return slic(im, n_segments = 100, sigma = 3) # 500, 5
 
-        ims_and_labels = zip(self.images, im_labels)
-        '''pool = ThreadPool(20) 
-        results = pool.map(self.im_superpixels, ims_and_labels)
-        pool.close() 
-        pool.join()''' # wait 
+	def im_superpixels(self,pair):
+		im = pair[0]
+		im_labels = pair[1]
+		unique_labels = np.unique(im_labels)
+		segs = [self.pixels_by_label(im,im_labels,x) for x in unique_labels]
+		return np.vstack([self.seg_2_stats(seg) for seg in segs])
+	
+	def pixels_by_label(self, im, im_labels, x):
+		idxs = np.argwhere(im_labels == x)
+		return im[idxs[:,0],idxs[:,1],:]
+	
+	def seg_2_stats(self, seg):
+		return np.array([np.mean(seg[:,0]), np.std(seg[:,0]), np.median(seg[:,0]),
+				np.mean(seg[:,1]), np.std(seg[:,1]), np.median(seg[:,1]),
+				np.mean(seg[:,2]), np.std(seg[:,2]), np.median(seg[:,2])])
+	def im_mask(self,pair):
+		im_labels = pair[0]
+		mask = pair[1]
+		unique_labels = np.unique(im_labels)
+		return [self.mask_by_label(mask,im_labels,x) for x in unique_labels]
+	
+	def mask_by_label(self, mask, im_labels, x):
+		idxs = np.argwhere(im_labels == x)
+		mode = stats.mode(mask[idxs[:,0],idxs[:,1]])
+		return mode.mode[0]
 
-	results = [self.im_superpixels(pair) for pair in ims_and_labels]
+class SuperPxlParallelTransform(ITransform):
+	def transform(self):
+		st = time.time()
+		self.y =[]
+		pool = ThreadPool(20) 
 
-        self.X = np.concatenate(results, axis=0)
-	print '2 => ' + str(time.time()-st)
+		im_labels = pool.map(self.get_im_labels, self.images)
+		pool.close() 
+		pool.join() # wait 
 
-        pool = ThreadPool(20) 
-        labels_and_masks = zip(im_labels, [self.im_2_mask(m) for m in self.masks])
-        yy = pool.map(self.im_mask, labels_and_masks)
-        pool.close() 
-        pool.join() # wait 
+		print '1 => ' + str(time.time()-st)
 
-        self.y = np.concatenate(yy, axis=0)
-	print '3 => ' + str(time.time()-st)
-        #self.X = np.concatenate([self.im_superpixels(pair) for pair in images_and_masks], axis=0)
-        #self.y = np.concatenate([self.im_mask(mask) for mask in self.masks], axis=0)
-        # self.y = np.ones(self.X.shape[0])
-    def get_im_labels(self,im):
-        return slic(im, n_segments = 100, sigma = 3) # 500, 5
+		ims_and_labels = zip(self.images, im_labels)
+		'''pool = ThreadPool(20) 
+		results = pool.map(self.im_superpixels, ims_and_labels)
+		pool.close() 
+		pool.join()''' # wait 
 
-    def im_superpixels(self,pair):
-	im = pair[0]
-	im_labels = pair[1]
-        #im_labels = slic(im, n_segments = 100, sigma = 3) # 500, 5
-        unique_labels = np.unique(im_labels)
-	#for x in unique_labels:
-        #    idx = np.argwhere(im_labels == x)
-        #    print x
-        #    print idx.shape
-        #    print im[idx].shape
-        segs = [self.pixels_by_label(im,im_labels,x) for x in unique_labels]
-        return np.vstack([self.seg_2_stats(seg) for seg in segs])
-    def pixels_by_label(self, im, im_labels, x):
-        idxs = np.argwhere(im_labels == x)
-        #index = self.images.index(im)
-        #mask = self.masks[index]
-        #mode = stats.mode(mask[idxs[:,0],idxs[:,1]])
-        #self.y.append(mode.mode[0])
-	#print mode
-        return im[idxs[:,0],idxs[:,1],:]
-    def seg_2_stats(self, seg):
-        #print seg.shape
-        return np.array([np.mean(seg[:,0]), np.std(seg[:,0]), np.median(seg[:,0]),
-			 np.mean(seg[:,1]), np.std(seg[:,1]), np.median(seg[:,1]),
-                         np.mean(seg[:,2]), np.std(seg[:,2]), np.median(seg[:,2])])
-    def im_mask(self,pair):
-        im_labels = pair[0]
-        mask = pair[1]
-        unique_labels = np.unique(im_labels)
-        return [self.mask_by_label(mask,im_labels,x) for x in unique_labels]
-        #return np.vstack([self.seg_2_stats(seg) for seg in segs])
-    def mask_by_label(self, mask, im_labels, x):
-        idxs = np.argwhere(im_labels == x)
-        mode = stats.mode(mask[idxs[:,0],idxs[:,1]])
-        return mode.mode[0]
+		results = [self.im_superpixels(pair) for pair in ims_and_labels]
+
+		self.X = np.concatenate(results, axis=0)
+		print '2 => ' + str(time.time()-st)
+
+		pool = ThreadPool(20) 
+		labels_and_masks = zip(im_labels, [self.im_2_mask(m) for m in self.masks])
+		yy = pool.map(self.im_mask, labels_and_masks)
+		pool.close() 
+		pool.join() # wait 
+
+		self.y = np.concatenate(yy, axis=0)
+		print '3 => ' + str(time.time()-st)
--- testing/test2.py
+++ testing/test2.py
@@ -3,7 +3,8 @@ start = time.time()
 
 import sys
 import Transform
-import skimage.io as io
+import skimage.io as ioi
+import numpy as np
 from sklearn.metrics import mean_absolute_error
 from ELM import ELMRegressor
 
@@ -19,7 +20,7 @@ path = '' if len(sys.argv) < 3 else str(sys.argv[2])
 # load all training images @ path
 all_train_masks = io.ImageCollection(path + '*.bmp')
 
-train = Transform.SuperPxlTransform(all_train_imgs, all_train_masks)
+train = Transform.SuperPxlParellelTransform(all_train_imgs, all_train_masks)
 train.transform()
 X_train = train.X
 y_train = train.y
@@ -33,14 +34,20 @@ all_test_imgs = io.ImageCollection(path + '*.jpg')
 path = '' if len(sys.argv) < 5 else str(sys.argv[4])
 all_test_masks = io.ImageCollection(path + '*.bmp')
 
-test = Transform.SuperPxlTransform(all_test_imgs, all_test_masks)
+test = Transform.SuperPxlParallelTransform(all_test_imgs, all_test_masks)
 test.transform()
 X_test = test.X
 y_test = test.y
 
 print 'done, transforming test data: ' + str(time.time()-start)
 
-ELM = ELMRegressor(30)
+# same transformed data
+np.savetxt("X_train.csv", X_train, delimiter=",")
+np.savetxt("y_train.csv", y_train, delimiter=",")
+np.savetxt("X_test.csv", X_test, delimiter=",")
+np.savetxt("y_test.csv", y_test, delimiter=",")
+
+ELM = ELMRegressor(2000)
 ELM.fit(X_train, y_train)
 prediction = ELM.predict(X_train)
 
